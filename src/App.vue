fc6ce3023ec5723e93be188b9102d84daeb08c73
Luiggino Obreque Minio <luiggino.obreque@axity.com>
2022-02-02 10:02:55 -0300

    added element ui, form sample

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <form-sample></form-sample>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FormSample from "@/components/FormSample";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FormSample
   }
 }
 </script>
